49e455fe4d744228dfd3ea2664df3ae6ceb44055
Frank Lichtenheld <frank@lichtenheld.com>
2022-02-23 13:39:14 +0100

    ps_support.ps1: Add file for support functions
    Start with allowing us to do better error checks by
    defining CheckLastErrorCode. Based on
    https://stackoverflow.com/a/70355330/10681123
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

---
+++ buildbot-host/scripts/ps_support.ps1
@@ -0,0 +1,15 @@
+Set-StrictMode -Version Latest
+function CheckLastExitCode {
+    param ([int[]]$SuccessCodes = @(0))
+
+    if (!$?) {
+        Write-Host "Last CMD failed" -ForegroundColor Red
+        exit 1
+    }
+
+    if ((Test-Path variable:\lastExitCode) `
+      -and ($SuccessCodes -notcontains $lastExitCode)) {
+        Write-Host "EXE RETURNED EXIT CODE $lastExitCode" -ForegroundColor Red
+        exit $lastExitCode
+    }
+}
