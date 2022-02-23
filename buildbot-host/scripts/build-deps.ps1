1a60c6fffdcd8996a6f4af3b2f120c96170f9b87
Frank Lichtenheld <frank@lichtenheld.com>
2022-02-23 14:19:16 +0100

    build-deps.ps1: Use CheckLastExitCode
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- buildbot-host/scripts/build-deps.ps1
+++ buildbot-host/scripts/build-deps.ps1
@@ -1,27 +1,34 @@
 param ([string] $workdir)
 
+. C:\Windows\Temp\ps_support.ps1
+
 Write-Host "Setting up openvpn build dependencies with vcpkg"
 
 if (-Not (Test-Path $workdir)) {
   New-Item -Type directory $workdir
+  CheckLastExitCode
 }
 
 if (-Not (Test-Path "${workdir}\openvpn")) {
   & git.exe clone -b master https://github.com/OpenVPN/openvpn.git "${workdir}\openvpn"
+  CheckLastExitCode
 }
 
 if (-Not (Test-Path "${workdir}\openvpn-build")) {
   & git.exe clone https://github.com/OpenVPN/openvpn-build.git "${workdir}\openvpn-build"
+  CheckLastExitCode
 }
 
 if (-Not (Test-Path "${workdir}\openvpn-gui")) {
   & git.exe clone https://github.com/OpenVPN/openvpn-gui.git "${workdir}\openvpn-gui"
+  CheckLastExitCode
 }
 
 # Install OpenVPN build dependencies
 $architectures = @('x64','x86','arm64')
 foreach ($arch in $architectures) {
     & "${workdir}\vcpkg\vcpkg.exe" --overlay-ports="${workdir}\openvpn\contrib\vcpkg-ports" --overlay-triplets="${workdir}\openvpn\contrib\vcpkg-triplets" install "lz4:${arch}-windows-ovpn" "lzo:${arch}-windows-ovpn" "openssl3:${arch}-windows-ovpn" "pkcs11-helper:${arch}-windows-ovpn" "tap-windows6:${arch}-windows-ovpn"
+    CheckLastExitCode
 }
 
 # Ensure that we can convert the man page from rst to html
