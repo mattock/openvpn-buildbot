b1bb1b56db262fdd9f4d86295f44bed28f566bd4
Frank Lichtenheld <frank@lichtenheld.com>
2022-04-28 13:59:35 +0200

    create-buildbot-user.ps1: Set password to never expire
    When using the buildnode for more than 42 days (the default
    password expiry in Windows Server 2019) it becomes inoperable.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- scripts/create-buildbot-user.ps1
+++ scripts/create-buildbot-user.ps1
@@ -1,5 +1,5 @@
 param ([string] $password)
 
 $buildbot_user_password = ConvertTo-SecureString $password -AsPlainText -Force
-New-LocalUser -AccountNeverExpires -Description "buildbot" -FullName "buildbot" -Name "buildbot" -Password $buildbot_user_password
+New-LocalUser -AccountNeverExpires -PasswordNeverExpires -Description "buildbot" -FullName "buildbot" -Name "buildbot" -Password $buildbot_user_password
 & net localgroup administrators buildbot /add
