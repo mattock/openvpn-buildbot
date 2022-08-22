af3c40e78cde01b6b87dc4c96570657dbc9653de
Frank Lichtenheld <frank@lichtenheld.com>
2022-08-22 13:14:30 +0200

    alpine: fix and enable
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- buildbot-host/scripts/install-openvpn-build-deps-alpine.sh
+++ buildbot-host/scripts/install-openvpn-build-deps-alpine.sh
@@ -4,20 +4,25 @@ apk add \
 autoconf \
 autoconf-archive \
 automake \
+bash \
 cmake \
+cmocka-dev \
 curl \
 fping \
 g++ \
 git \
 iproute2 \
 jsoncpp-dev \
+libcap-ng-dev \
 libtool \
 libuuid \
 libxml2-dev \
+linux-headers \
 linux-pam-dev \
 lz4-dev \
 lzo-dev \
 make \
+mbedtls-dev \
 net-tools \
 pkgconf \
 opensc-dev \
@@ -30,4 +35,8 @@ py3-pip \
 py3-roman \
 py3-setuptools \
 py3-wheel \
+shadow \
 tinyxml2-dev
+
+# alpine doesn't have fping6 symlink, t_client.sh can't deal
+ln -s /usr/sbin/fping /usr/sbin/fping6
