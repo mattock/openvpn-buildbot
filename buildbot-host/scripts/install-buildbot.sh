a0ad5c0948a2423c7cc926d9251cdc72c48ead65
Frank Lichtenheld <frank@lichtenheld.com>
2023-03-03 23:04:42 +0100

    debian-unstable: Fix install-buildbot.sh with new pip
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- buildbot-host/scripts/install-buildbot.sh
+++ buildbot-host/scripts/install-buildbot.sh
@@ -7,9 +7,9 @@ set -ex
 BUILDBOT_VERSION=3.5.0
 
 # hardcode cryptography version since newer don't work on CentOS 7
-pip3 install --upgrade pip && \
-pip --no-cache-dir install cryptography==37.0.4 && \
-pip --no-cache-dir install twisted[tls] && \
-pip --no-cache-dir install buildbot_worker==$BUILDBOT_VERSION
+pip3 install $PIP_INSTALL_OPTS --upgrade pip
+pip --no-cache-dir install $PIP_INSTALL_OPTS cryptography==37.0.4
+pip --no-cache-dir install $PIP_INSTALL_OPTS twisted[tls]
+pip --no-cache-dir install $PIP_INSTALL_OPTS buildbot_worker==$BUILDBOT_VERSION
 
 useradd --create-home --home-dir=/var/lib/buildbot buildbot
