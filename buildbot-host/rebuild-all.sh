df3b950af825a8a638e94e4d87820c27d618915c
Frank Lichtenheld <frank@lichtenheld.com>
2023-05-03 16:08:48 +0200

    rebuild-all.sh: Use set -eux
    Abort on errors. Otherwise they just get lost in
    the log.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- buildbot-host/rebuild-all.sh
+++ buildbot-host/rebuild-all.sh
@@ -2,6 +2,8 @@
 #
 # Rebuild all containers (master and workers)
 
+set -eux
+
 # Check that we're in the right directory
 if ! [ -r "rebuild.sh" ]; then
     echo "ERROR: this script must be run in the directory with rebuild.sh!"
@@ -9,9 +11,9 @@ if ! [ -r "rebuild.sh" ]; then
 fi
 
 # Check every directory and rebuild if a file starting with Dockerfile is found
-for DOCKERFILE in `find -maxdepth 2 -type f -regextype egrep -regex '.*/(Dockerfile|Dockerfile.base)'`; do
-    DIR=`echo $DOCKERFILE|cut -d "/" -f 2`
-    ./rebuild.sh $DIR
+for DOCKERFILE in $(find -maxdepth 2 -type f -regextype egrep -regex '.*/(Dockerfile|Dockerfile.base)'); do
+    DIR=$(echo "$DOCKERFILE"|cut -d "/" -f 2)
+    ./rebuild.sh "$DIR"
 done
 
 # tends to produce several dangling images
