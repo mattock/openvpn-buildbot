e5ceb383aa7089fb037013118e55fa281c7f3188
Samuli Seppänen <samuli.seppanen@gmail.com>
2024-06-25 06:37:00 +0000

    Do not fail if pip cannot be upgraded in install-buildbot.sh
    This can happen on some operating systems like Ubuntu 24.04 when pip was
    installed from Debian packages.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- buildbot-host/scripts/install-buildbot.sh
+++ buildbot-host/scripts/install-buildbot.sh
@@ -7,7 +7,14 @@ set -ex
 BUILDBOT_VERSION=3.11.1
 
 # hardcode cryptography version since newer don't work on CentOS 7
-pip3 install $PIP_INSTALL_OPTS --upgrade pip
+
+# Upgrading pip may or may not work. On Ubuntu 24.04 it may fail with
+#
+# "ERROR: Cannot uninstall pip 24.0, RECORD file not found. Hint: The package was installed by debian."
+#
+# Try and if it fails just keep on going
+#
+pip3 install $PIP_INSTALL_OPTS --upgrade pip || true
 pip --no-cache-dir install $PIP_INSTALL_OPTS cryptography==37.0.4
 pip --no-cache-dir install $PIP_INSTALL_OPTS twisted[tls]
 pip --no-cache-dir install $PIP_INSTALL_OPTS buildbot_worker==$BUILDBOT_VERSION
