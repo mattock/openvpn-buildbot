85a1789c68ffd206b2f163799052d3c2f3aa99c3
Frank Lichtenheld <frank@lichtenheld.com>
2024-07-24 18:50:15 +0200

    vsbuildtools: Also install Clang
    We might sometimes prefer that compiler on Windows
    or want to test with both.
    
    Signed-off-by: Frank Lichtenheld <frank@lichtenheld.com>

--- scripts/vsbuildtools.ps1
+++ scripts/vsbuildtools.ps1
@@ -2,7 +2,7 @@ param ([string] $version = "2022")
 
 Write-Host "Installing Visual Studio build tools"
 & choco.exe install -y "visualstudio${version}buildtools"
-& choco.exe install -y "visualstudio${version}-workload-vctools" --params "--add Microsoft.VisualStudio.Component.UWP.VC.ARM64 --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --add Microsoft.VisualStudio.Component.VC.ATL.Spectre --add Microsoft.VisualStudio.Component.VC.ATLMFC.Spectre --add Microsoft.VisualStudio.Component.VC.ATL.ARM64.Spectre --add Microsoft.VisualStudio.Component.VC.MFC.ARM64.Spectre --add Microsoft.VisualStudio.Component.VC.Runtimes.ARM64.Spectre --add Microsoft.VisualStudio.Component.VC.Runtimes.x86.x64.Spectre --quiet"
+& choco.exe install -y "visualstudio${version}-workload-vctools" --params "--add Microsoft.VisualStudio.Component.UWP.VC.ARM64 --add Microsoft.VisualStudio.Component.VC.Tools.ARM64 --add Microsoft.VisualStudio.Component.VC.ATL.Spectre --add Microsoft.VisualStudio.Component.VC.ATLMFC.Spectre --add Microsoft.VisualStudio.Component.VC.ATL.ARM64.Spectre --add Microsoft.VisualStudio.Component.VC.MFC.ARM64.Spectre --add Microsoft.VisualStudio.Component.VC.Runtimes.ARM64.Spectre --add Microsoft.VisualStudio.Component.VC.Runtimes.x86.x64.Spectre --add Microsoft.VisualStudio.Component.VC.Llvm.Clang --add Microsoft.VisualStudio.Component.VC.Llvm.ClangToolset --add Microsoft.VisualStudio.Component.VC.ASAN --quiet"
 
 Write-Host "Installing Debugging Tools"
 & choco.exe install -y windows-sdk-10-version-2004-windbg
